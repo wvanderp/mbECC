542132395eb1eb8c10374b2f8681a34a97112a13
wouter van der plas <wouterv.dplas@gmail.com>
2015-08-03 20:59:59 +0200

    found a edit

--- api/enter.php
+++ api/enter.php
@@ -1,3 +1,14 @@
 <?php
 	require_once 'lib/mysql.php';
+	$query = "INSERT INTO remix (id, mbzid, done) VALUES "
+	$file = file_get_contents("out.txt");
+	$lines = explode("\n", $file);
+
+	foreach ($lines as $l) {
+		$query = $query."(NULL, ".$l.", 0), "
+	}
+
+	$query = substr($query, 0, strlen($query) - 3);
+
+	echo $query;
 ?>
--- api/getRemix.php
+++ api/getRemix.php
@@ -1,3 +1,8 @@
 <?php
 	require_once 'lib/mysql.php';
+	$query = "SELECT * from remix WHERE done = 0 limit 1";
+
+	$result = mysqli_query($link, $query) or die(mysqli_error($link));
+	
+	echo json_encode(mysqli_fetch_assoc($result);
 ?>
